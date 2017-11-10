eaacfef029e777e8a516cdc0ae74b0769772b2e8
Eddy Yao <eddy2yao2@gmail.com>
2017-11-10 14:29:39 -0500

    Cleaning up and repairing from OS reset

--- app/src/main/java/com/example/eddy/servr/Activities/HomeActivity.java
+++ app/src/main/java/com/example/eddy/servr/Activities/HomeActivity.java
@@ -1,13 +1,10 @@
 package com.example.eddy.servr.Activities;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.example.eddy.servr.R;
