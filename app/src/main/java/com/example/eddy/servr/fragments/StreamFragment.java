0865d6c9e59742e8b6c677c8dc03c35e3e6d870e
Eddy Yao <eddy2yao2@gmail.com>
2017-11-22 14:28:48 -0500

    Implemented custom adapter under service_item - OnClickListeners attached to each ListView item

--- app/src/main/java/com/example/eddy/servr/fragments/StreamFragment.java
+++ app/src/main/java/com/example/eddy/servr/fragments/StreamFragment.java
@@ -1,6 +1,10 @@
 package com.example.eddy.servr.fragments;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -9,7 +13,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.eddy.servr.R;
 
@@ -30,7 +37,7 @@ public class StreamFragment extends Fragment{
                 "Test Case 11", "Test Case 12", "Test Case 13"));
 
         if (getActivity() != null) {
-            ArrayAdapter adapter = new ArrayAdapter<>(getActivity(), R.layout.service_item, serviceNames);
+            StreamAdapter adapter = new StreamAdapter(serviceNames, getContext());
             ListView listView = stream.findViewById(R.id.StreamListView);
             try {
                 listView.setAdapter(adapter);
@@ -40,5 +47,64 @@ public class StreamFragment extends Fragment{
         }
 
         return stream;
+    }
+
+    public class StreamAdapter extends BaseAdapter implements ListAdapter {
+        private ArrayList<String> list = new ArrayList<>();
+        private Context context;
+
+        private StreamAdapter(ArrayList<String> list, Context context) {
+            this.list = list;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return list.get(pos);
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return 0;
+        }
+
+        @SuppressLint("InflateParams")
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                assert inflater != null;
+                view = inflater.inflate(R.layout.service_item, null);
+            }
+
+            //Handle TextView and display string from your list
+            TextView listItemText = view.findViewById(R.id.list_item_string);
+            listItemText.setText(list.get(position));
+
+            TextView listItemPrice = view.findViewById(R.id.list_item_price);
+            listItemPrice.setText("$Test Price");
+
+            view.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    new AlertDialog.Builder(getContext())
+                            .setMessage("Sorry this feature hasn't been implemented yet!")
+                            .setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            })
+                            .show();
+                }
+            });
+
+            return view;
+        }
     }
 }
