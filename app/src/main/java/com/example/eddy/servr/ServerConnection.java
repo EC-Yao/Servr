9e613e4e07e0c13784d665678ab107d72e445be4
Eddy Yao <eddy2yao2@gmail.com>
2018-01-10 13:41:06 -0500

    Testing data handling

--- app/src/main/java/com/example/eddy/servr/ServerConnection.java
+++ app/src/main/java/com/example/eddy/servr/ServerConnection.java
@@ -17,10 +17,17 @@ public class ServerConnection {
     private static String temp;
     public static ArrayList<String> user;
 
+    private static final String input = "[[1, 1, Alpha Tester, Testing for alpha, $1.99], [2, 1, Alpha Alpha Tester, Testing for alpha alpha, $11.99], [4, 3, Cheesecake Eater, Eating cheesecake professionally, $2.99]]";
+    private static String output;
+    private static ArrayList<ArrayList<String>> printedOutput;
+
+
     public ServerConnection(){
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         sendMessage("Ping");
+
+        System.out.println(getNestedArray(input));
     }
 
     public void sendMessage(String message){
@@ -68,11 +75,27 @@ public class ServerConnection {
         try{
             //@school: 10.178.155.72
             //@home: 192.168.2.13
-            socket = new Socket("10.178.155.72", 8001);
+            socket = new Socket("192.168.2.13", 8001);
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (Exception e) {
-            System.out.println(Arrays.toString(e.getStackTrace()));
+            System.out.println(Arrays.asList(e.getStackTrace()));
         }
+    }
+
+    private ArrayList<ArrayList<String>> getNestedArray(String inputArray){
+        printedOutput = new ArrayList<>();
+        output = inputArray.substring(1, inputArray.length() - 1);
+
+        while (output.contains("]")){
+            printedOutput.add(new ArrayList<>(Arrays.asList(output.substring(1, output.indexOf("]")).split(","))));
+            if (output.indexOf("]") < output.length() - 1){
+                output = output.substring(output.indexOf("]") + 2);
+            } else {
+                break;
+            }
+        }
+
+        return printedOutput;
     }
 }
